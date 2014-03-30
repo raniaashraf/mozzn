693c9ecb69cc130384168bb428729993dc39465a
raniaashraf <rania@overcstudios.com>
2014-03-30 16:33:31 +0200

    Updating Gem 2014-03-30 16:33:31

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 Bundler.setup
 
-require 'mozzn' # and any other gems you need
+# require 'your_gem_name' # and any other gems you need
 
 RSpec.configure do |config|
   # some (optional) config here
