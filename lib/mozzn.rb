dd3241d92fbdfb198ee8a5b975652a6907eff526
raniaashraf <rania@overcstudios.com>
2014-03-31 12:04:25 +0200

    Updating Gem 2014-03-31 12:04:25

--- lib/mozzn.rb
+++ lib/mozzn.rb
@@ -3,21 +3,16 @@ require "mozzn/version"
 require 'mozzn/config'
 require 'mozzn/api'
 require 'mozzn/cli'
-require 'mozzn/logger'
 
 
 module Mozzn
   class << self
-    attr_accessor   :debug, :log,:config
-    def start debug
-      @debug = debug
-      # Initialize log first
-      @log = Mozzn::Logger.new
+    attr_accessor :config
+    def start 
       # Now load the congiuration
       @config = Mozzn::Config.new
 
       @chimera = Mozzn::Api.new
-
       # Invoke our CLI
       Mozzn::CLI.start
     end
