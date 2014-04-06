a7d4b7a4f80bfa8be18542e8ff09db39d251abf0
raniaashraf <rania@overcstudios.com>
2014-04-06 13:10:48 +0200

    Updating Gem 2014-04-06 13:10:48

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -153,7 +153,7 @@ module Mozzn
       @connection = Faraday.new('https://rubygems.org/api/v1/versions/coulda.json')
         response = @connection.get 
         JSON.parse(response.body)
-        puts "response: #{response.inspect}"
+        puts "response: #{response['number'].inspect}"
     end 
 
     desc 'help COMMAND', 'For more infromation about spicific COMMAND'
