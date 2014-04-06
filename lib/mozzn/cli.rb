872cd29ce2f37585190686b3db6e8b4f504c78d1
raniaashraf <rania@overcstudios.com>
2014-04-06 15:39:30 +0200

    Updating Gem 2014-04-06 15:39:30

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -153,7 +153,7 @@ module Mozzn
       @connection = Faraday.new('https://rubygems.org/api/v1/versions/coulda.json')
         response = @connection.get 
         body = JSON.parse(response.body)
-        say body.map { |n| n['number'] }.join("\n"), :green
+        say body.map({ |n| n['number'] }).join("\n"), :green
     end 
 
     desc 'help COMMAND', 'For more infromation about spicific COMMAND'
