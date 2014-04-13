6f39add18a727ef06259461c7219333b6214e909
raniaashraf <rania@overcstudios.com>
2014-04-13 15:30:43 +0200

    Updating Gem 2014-04-13 15:30:43

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -212,7 +212,7 @@ module Mozzn
       path = 'applications/resources'
       begin
       response = mozzn.get(path, params)
-        say response['resources_name'], :green 
+        say "Resources name: #{response['resources_name']}", :green 
         #TODO resources with the form 'name' => name , 'command' => command  
       rescue JSON::ParserError => e
         raise Thor::Error,"You do not have an application with the name #{params[:appname]}. Please check the application name."
