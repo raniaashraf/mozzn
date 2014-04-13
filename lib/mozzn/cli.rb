3a6907467d841276efcffc2a27c216dcea6d45bf
raniaashraf <rania@overcstudios.com>
2014-04-13 15:46:50 +0200

    Updating Gem 2014-04-13 15:46:50

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -213,6 +213,7 @@ module Mozzn
       path = 'applications/resources'
       begin
       response = mozzn.get(path, params)
+      say response['resources'], :red
       table = Terminal::Table.new(headings: ['Process', 'Command']) do |t|
         response['resources'].each do |resource|
           t.add_row [resource['resources'], resource['resources']]
