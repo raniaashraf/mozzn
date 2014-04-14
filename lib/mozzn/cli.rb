4df4b8aa293d620a74b6e66de6229a4971733828
raniaashraf <rania@overcstudios.com>
2014-04-14 14:34:14 +0200

    Updating Gem 2014-04-14 14:34:14

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -135,16 +135,23 @@ module Mozzn
       params = {
         name: name
       }
-      path = 'applications/remove'
+      search_path = "applications/search"
       begin
-      response = mozzn.get(path, params)
-        say response['info'], :green   
+      response = mozzn.get(search_path, params)
+      if response.has_key?('info')
+        raise Thor::Error, "#{response['info']}"
+      else
+        id = response['app_id']
+        resources_path = "applications/#{id}/remove"
+        response = mozzn.get(resources_path,nil)
+        say response['info'], :green 
+      end  
       rescue JSON::ParserError => e
         raise Thor::Error,"You do not have an application with the name #{params[:name]}. Please check the application name."
       end
-    rescue Mozzn::Disconnected
+      rescue Mozzn::Disconnected
       say 'Unable to connect to Mozzn. Check your internet connection!', :red
-    rescue Mozzn::UnexpectedOutput
+      rescue Mozzn::UnexpectedOutput
       say 'UnexpectedOutput', :red
     end
 
