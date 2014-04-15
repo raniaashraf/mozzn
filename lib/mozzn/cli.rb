bc28245dd3a6c776810eeb6c0b3002fe9f0a18e6
raniaashraf <rania@overcstudios.com>
2014-04-15 13:41:05 +0200

    Updating Gem 2014-04-15 13:41:05

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -251,18 +251,19 @@ module Mozzn
         end
         instances_path = "applications/#{id}/instances"
         response = mozzn.get(instances_path,nil)
+        say response['instances'].inspect, :green
         if response.has_key?('info')
           raise Thor::Error, "#{response['info']}"
-        else
-          table3 = Terminal::Table.new(headings: ['Id', 'IP']) do |t|
-            response['instances'].each do |instant|
-              key = instant['body']['name']
-              value = instant['body']['ip_address']
-              if key.present?
-                t.add_row [key, value]
-              end
-            end
-          end
+        # else
+        #   table3 = Terminal::Table.new(headings: ['Id', 'IP']) do |t|
+        #     response['instances'].each do |instant|
+        #       key = instant['body']['name']
+        #       value = instant['body']['ip_address']
+        #       if key.present?
+        #         t.add_row [key, value]
+        #       end
+        #     end
+          # end
         end
         say "Processes:"
         say "#{table1}"
