5573ce0b49c701aebbe73147802d2d840f021b62
raniaashraf <rania@overcstudios.com>
2014-04-16 12:01:46 +0200

    Updating Gem 2014-04-16 12:01:46

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -287,7 +287,7 @@ module Mozzn
       File.open(".git/config", "r") do |f|
         @data = f.read
       end 
-      words = @data['url']
+      words = @data
       say words, :green
       # mozzn = Mozzn::Api.new(Mozzn::Config.new.read['token'])
     #   if !appname.present?
