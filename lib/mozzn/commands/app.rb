dfc1caa1c50cdd389c0cf9af687168b9128fef61
raniaashraf <rania@overcstudios.com>
2014-04-17 19:56:42 +0200

    Updating Gem 2014-04-17 19:56:42

--- lib/mozzn/commands/app.rb
+++ lib/mozzn/commands/app.rb
@@ -16,20 +16,24 @@ module Mozzn
           }
         }
         response = mozzn.post(path, params)
-        say response['info'], :green
-        git = Git.init
-        unless File.exist?('.git')
-            git.add(all: true)
-          begin
-            git.commit('First commit')
-          rescue Git::GitExecuteError => e
-            # Do nothing
+        if response.has_key?('message')
+          raise Thor::Error, "#{response['message']}"
+        else
+          say response['info'], :green
+          git = Git.init
+          unless File.exist?('.git')
+              git.add(all: true)
+            begin
+              git.commit('First commit')
+            rescue Git::GitExecuteError => e
+              # Do nothing
+            end
           end
-        end
-        begin
-          git.add_remote("mozzn", "git@git.mozzn.com:#{name}.git")
-        rescue Git::GitExecuteError => e
-          say 'Git remote already configured, skipping...'
+          begin
+            git.add_remote("mozzn", "git@git.mozzn.com:#{name}.git")
+          rescue Git::GitExecuteError => e
+            say 'Git remote already configured, skipping...'
+          end
         end
       rescue Mozzn::Disconnected
         say 'Unable to connect to Mozzn check the internet connection!', :red
