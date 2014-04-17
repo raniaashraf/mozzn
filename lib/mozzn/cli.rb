2ec963178fca3502df744843c59a36abb8262b31
raniaashraf <rania@overcstudios.com>
2014-04-17 16:19:06 +0200

    Updating Gem 2014-04-17 16:19:06

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -53,73 +53,13 @@ module Mozzn
       say 'UnexpectedOutput', :red
     end
 
-    desc "Key subcommand", "add key"
+    desc "Key subcommand", "List, add and remove keys."
     subcommand "key", Key
 
-    desc 'create_app APPNAME', 'create a new application'
-    # mozzn create_app
-    def create_app name = nil
-      mozzn = Mozzn::Api.new(Mozzn::Config.new.read['token'])
-      if name == nil
-        raise Thor::Error, "You must enter application name."
-      end
-      path = 'applications'
-      params = {
-        application: {
-          name: name
-        }
-      }
-      response = mozzn.post(path, params)
-      say response['info'], :green
-      git = Git.init
-      unless File.exist?('.git')
-          git.add(all: true)
-        begin
-          git.commit('First commit')
-        rescue Git::GitExecuteError => e
-          # Do nothing
-        end
-      end
-      begin
-        git.add_remote("mozzn", "git@git.mozzn.com:#{name}.git")
-      rescue Git::GitExecuteError => e
-        say 'You already have this remote.', :red
-      end
-    rescue Mozzn::Disconnected
-      say 'Unable to connect to Mozzn check the internet connection!', :red
-    rescue Mozzn::UnexpectedOutput
-      say 'UnexpectedOutput'
-    end
+    desc "App subcommand", "List, add and remove applications."
+    subcommand "app", App
 
-    desc 'remove_app APPNAME', 'Remove spcicfic Application.'
-
-    def remove_app name = nil
-      mozzn = Mozzn::Api.new(Mozzn::Config.new.read['token'])
-      if !name.present?
-        raise Thor::Error, "You must enter Application Name!"
-      end
-      params = {
-        name: name
-      }
-      search_path = "applications/search"
-      begin
-      response = mozzn.get(search_path, params)
-      if response.has_key?('info')
-        raise Thor::Error, "#{response['info']}"
-      else
-        id = response['app_id']
-        resources_path = "applications/#{id}/remove"
-        response = mozzn.get(resources_path,nil)
-        say response['info'], :green 
-      end  
-      rescue JSON::ParserError => e
-        raise Thor::Error,"You do not have an application with the name #{params[:name]}. Please check the application name."
-      end
-      rescue Mozzn::Disconnected
-      say 'Unable to connect to Mozzn. Check your internet connection!', :red
-      rescue Mozzn::UnexpectedOutput
-      say 'UnexpectedOutput', :red
-    end
+    
 
     desc 'update', 'To show if there is an update for the CLI'
     def update
