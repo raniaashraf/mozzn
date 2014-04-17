53ecfd03b0e2578763568e39780b1c27ff7f411c
raniaashraf <rania@overcstudios.com>
2014-04-17 23:25:57 +0200

    Updating Gem 2014-04-17 23:25:57

--- lib/mozzn/commands/app.rb
+++ lib/mozzn/commands/app.rb
@@ -83,7 +83,7 @@ module Mozzn
       def resources 
         # begin
           mozzn = Mozzn::Api.new(Mozzn::Config.new.read['token'])
-          puts mozzn 
+          puts Mozzn::Config.new.read['token'] 
         # rescue Mozzn::UnexpectedOutput => e
         #   say 'You need to login in order to continue.', :red
         # end
