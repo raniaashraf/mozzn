de5fcab77347cfcf57e9f564adb96616bcef7f74
raniaashraf <rania@overcstudios.com>
2014-04-17 14:32:02 +0200

    Updating Gem 2014-04-17 14:32:02

--- lib/mozzn/cli.rb
+++ lib/mozzn/cli.rb
@@ -57,42 +57,41 @@ module Mozzn
     method_option :public_key, :aliases => "-k", :desc => "RSA/DSA public key"
     method_option :key_path, :aliases => "-p", :desc => "Path to RSA/DSA public key"
     # mozzn add_key
-    namespace       "talk"
-    def key
-      
-      mozzn = Mozzn::Api.new(Mozzn::Config.new.read['token'])
-      if options[:key_path].present?
-        key_path = File.expand_path(options[:key_path])
-      elsif options[:public_key].present?
-        public_key = options[:public_key]
-      else
-        raise Thor::Error, "Neither a key path or an SSH key were provided. You must use -p or -k options."
-      end
-
-      if public_key.nil?
-        if File.exist?(key_path)
-          File.open(key_path, "rb") do |f|
-            public_key = f.read
-          end
+    class Add < thor
+      def key 
+        mozzn = Mozzn::Api.new(Mozzn::Config.new.read['token'])
+        if options[:key_path].present?
+          key_path = File.expand_path(options[:key_path])
+        elsif options[:public_key].present?
+          public_key = options[:public_key]
         else
-          raise Thor::Error, "Unable to read #{key_path}. File does not exist or not accessible."
+          raise Thor::Error, "Neither a key path or an SSH key were provided. You must use -p or -k options."
         end
-      end
 
-      path = 'keys'
-      params = {
-        key: {
-          public: public_key
+        if public_key.nil?
+          if File.exist?(key_path)
+            File.open(key_path, "rb") do |f|
+              public_key = f.read
+            end
+          else
+            raise Thor::Error, "Unable to read #{key_path}. File does not exist or not accessible."
+          end
+        end
+
+        path = 'keys'
+        params = {
+          key: {
+            public: public_key
+          }
         }
-      }
-      response = mozzn.post(path, params)
-      say response['info'], :green
-    rescue Mozzn::Disconnected
-      say 'Unable to connect to Mozzn check the internet connection!', :red
-    rescue Mozzn::UnexpectedOutput
-      say 'UnexpectedOutput', :red
+        response = mozzn.post(path, params)
+        say response['info'], :green
+      rescue Mozzn::Disconnected
+        say 'Unable to connect to Mozzn check the internet connection!', :red
+      rescue Mozzn::UnexpectedOutput
+        say 'UnexpectedOutput', :red
+      end
     end
-
     desc 'create_app APPNAME', 'create a new application'
     # mozzn create_app
     def create_app name = nil
