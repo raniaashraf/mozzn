fe0986122fa6689fa7d61e81d58919ca078c00c9
raniaashraf <rania@overcstudios.com>
2014-04-20 16:06:16 +0200

    Updating Gem 2014-04-20 16:06:16

--- lib/mozzn/commands/app.rb
+++ lib/mozzn/commands/app.rb
@@ -86,14 +86,14 @@ module Mozzn
       desc "list", "List all your Applications"
       def list
         token = Mozzn::Config.new.read['token']
-        say token.inspect, :green
-        # if token.nil?
-        #   raise Thor::Error,"You need to login in order to continue."
-        # end
-        # mozzn = Mozzn::Api.new(token)
-        # path = "applications"
-        # response = mozzn.get(path, nil) 
-        # say response, :green
+        # say token.inspect, :green
+        if token.nil?
+          raise Thor::Error,"You need to login in order to continue."
+        end
+        mozzn = Mozzn::Api.new(token)
+        path = "applications"
+        response = mozzn.get(path, nil) 
+        say response, :green
 
       end
 
